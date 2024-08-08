02d92c820fb89fd94334c9304059d41925b96080
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-07 23:19:24 -0300

    <Fix> just with beautifullSoup dont get the value (dynamic js) so we update to use Selenium (not the best option but the option we had)

--- Jobs_scrapping/Kabum_web_scrappy.py
+++ Jobs_scrapping/Kabum_web_scrappy.py
@@ -1,87 +1,104 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options
 import pandas as pd
+import time
 from datetime import datetime
-from utils import known_brands
+import os
 
-# URLs base para as categorias
-category_urls = {
-   # "Celular & Smartphone": "https://www.kabum.com.br/celular-smartphone/smartphones"
-    #"TV": "https://www.kabum.com.br/tv",
-   # "Tablets, iPads e E-readers": "https://www.kabum.com.br/tablets-ipads-e-readers",
-   "Computador": "https://www.kabum.com.br/computadores/notebooks"
-}
+# Lista de marcas conhecidas
+known_brands = [
+    "ACER", "ASUS", "SAMSUNG", "Dell", "Positivo", "Lenovo", "VAIO",
+    "HP", "Apple", "Multilaser", "Anvazise", "ASHATA", "Santino", "MSI",
+    "Marca Fácil", "Microsoft", "AWOW", "Gateway", "Compaq", "DAUERHAFT",
+    "SGIN", "Luqeeg", "Kiboule", "LG", "Panasonic", "Focket", "Toughbook",
+    "LTI", "GIGABYTE", "Octoo", "Chip7 Informática", "GLOGLOW", "GOLDENTEC",
+    "KUU", "HEEPDD", "Adamantiun", "Naroote", "Jectse", "Heayzoki",
+    "Motorola", "Xiaomi", "Nokia", "Poco", "realme", "Infinix", "Blu",
+    "Gshield", "Geonav", "Redmi", "Gorila Shield", "intelbras", "TCL",
+    "Tecno", "Vbestlife", "MaiJin", "SZAMBIT", "Otterbox", "Sony"
+]
 
-def collect_data_from_page(url, current_product):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+def initialize_driver(gecko_path, headless=True):
+    firefox_options = Options()
+    if headless:
+        firefox_options.add_argument("--headless")
     
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
+    service = FirefoxService(executable_path=gecko_path)
+    driver = webdriver.Firefox(service=service, options=firefox_options)
+    return driver
+
+def collect_data_from_page(driver):
     products = []
-    product_elements = soup.select(".productCard")
-    
-    if not product_elements:
-        print("No product elements found.")
+    product_elements = driver.find_elements(By.CSS_SELECTOR, "article.sc-9d1f1537-7.hxuzLm.productCard")
     
     for item in product_elements:
         try:
-            title_element = item.select_one(".nameCard")
-            price_element = item.select_one(".priceCard")
-            rating_element = item.select_one(".estrelaAvaliacao")
-            free_freight_element = item.select_one(".productCard-freeFreight")
+            price_element = item.find_element(By.CSS_SELECTOR, "span.priceCard")
+            name_element = item.find_element(By.CSS_SELECTOR, "span.nameCard")
+            link_element = item.find_element(By.CSS_SELECTOR, "a")
 
-            title = title_element.text.strip() if title_element else "No title"
-            price = price_element.text.strip() if price_element else "No price"
-            rating = rating_element.text.strip() if rating_element else 'No rating'
-            free_freight = free_freight_element and "Frete grátis" in free_freight_element.text
+            price = price_element.text
+            name = name_element.text
+            link = link_element.get_attribute("href")
 
             brand = "Unknown"
             for known_brand in known_brands:
-                if known_brand.lower() in title.lower():
+                if known_brand.lower() in name.lower():
                     brand = known_brand
                     break
 
             products.append({
-                'title': title,
+                'name': name,
                 'price': price,
                 'brand': brand,
-                'rating': rating,
-                'free_freight': free_freight,
-                'category': current_product,
+                'link': link,
+                'free_freight': False,  # KaBuM! não tem frete grátis
                 'CreatedAt': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                 'UpdatedAt': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                'website': 'Kabum'
+                'website': 'KaBuM'
             })
         except Exception as e:
-            print(f"Error: {e}")
             continue
     return products
 
-def scrape_kabum(base_url, current_product, num_pages=1):
+def scrape_kabum(gecko_path, base_url, num_pages=1, headless=True):
+    driver = initialize_driver(gecko_path, headless)
+    
     all_products = []
     
     for page in range(1, num_pages + 1):
         url = f"{base_url}?page_number={page}"
-        products = collect_data_from_page(url, current_product)
+        driver.get(url)
+        
+        time.sleep(5)
+        
+        products = collect_data_from_page(driver)
         all_products.extend(products)
+    
+    driver.quit()
     
     return pd.DataFrame(all_products)
 
-def main(categories):
-    all_data = pd.DataFrame()
-    num_pages = 1  
+def main():
+    gecko_path = os.getenv('Driver')
+    category_urls = {
+        # "Celular & Smartphone": "https://www.kabum.com.br/celular-smartphone/smartphones",
+        # "TV": "https://www.kabum.com.br/tv",
+        # "Tablets, iPads e E-readers": "https://www.kabum.com.br/tablets-ipads-e-readers",
+        "Computador": "https://www.kabum.com.br/computadores/notebooks"
+    }
+    num_pages = 1
 
-    for category, base_url in categories.items():
-        df = scrape_kabum(base_url, category, num_pages)
+    all_data = pd.DataFrame()
+
+    for category, base_url in category_urls.items():
+        df = scrape_kabum(gecko_path, base_url, num_pages, headless=True)
         all_data = pd.concat([all_data, df], ignore_index=True)
 
-    if all_data.empty:
-        print("Empty data")
-    else:
-        print(all_data.to_string(index=False))
+    # Printar os dados
+    print(all_data.to_string(index=False))
 
 if __name__ == "__main__":
-    main(category_urls)
+    main()
