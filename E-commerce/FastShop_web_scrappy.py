b8418a7706a09d734901ba0beefff40546644db3
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-13 21:51:47 -0300

    <Fix> FastShop identation

--- E-commerce/FastShop_web_scrappy.py
+++ E-commerce/FastShop_web_scrappy.py
@@ -22,11 +22,8 @@ def scroll_to_load_products(driver, timeout=10, scroll_pause_time=2):
     last_height = driver.execute_script("return document.body.scrollHeight")
     
     while True:
-        # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # Wait to load the page
         time.sleep(scroll_pause_time)
-        # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
