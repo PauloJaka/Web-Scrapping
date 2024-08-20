7ddf3fa5d3f9d5f8f95c1995917304fcbed09b4e
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-19 23:39:21 -0300

    <Feat> More brands on known brands

--- E-commerce/utils.py
+++ E-commerce/utils.py
@@ -1,12 +1,16 @@
 known_brands = [
-    "ACER", "ASUS", "SAMSUNG", "Dell", "Positivo", "Lenovo", "VAIO",
-    "HP", "Apple", "Multilaser", "Anvazise", "ASHATA", "Santino", "MSI",
-    "Marca Fácil", "Microsoft", "AWOW", "Gateway", "Compaq", "DAUERHAFT",
-    "SGIN", "Luqeeg", "Kiboule", "LG", "Panasonic", "Focket", "Toughbook",
-    "LTI", "GIGABYTE", "Octoo", "Chip7 Informática", "GLOGLOW", "GOLDENTEC",
-    "KUU", "HEEPDD", "Adamantiun", "Naroote", "Jectse", "Heayzoki", "Galaxy",
-    "Motorola", "SAMSUNG", "Xiaomi", "Apple", "Multilaser", "Positivo", "Nokia",
-    "Poco", "realme", "Infinix", "ASUS", "Blu", "Gshield", "Geonav", "Redmi",
-    "Gorila Shield", "intelbras", "TCL", "Tecno", "Vbestlife", "MaiJin", "SZAMBIT",
-    "Otterbox", "Sony"
+    "ACER", "ASUS", "SAMSUNG", "Dell", "Positivo", "Lenovo", "VAIO", "HP", "Apple", 
+    "Multilaser", "Anvazise", "ASHATA", "Santino", "MSI", "Marca Fácil", "Microsoft", 
+    "AWOW", "Gateway", "Compaq", "DAUERHAFT", "SGIN", "Luqeeg", "Kiboule", "LG", 
+    "Panasonic", "Focket", "Toughbook", "LTI", "GIGABYTE", "Octoo", "Chip7 Informática", 
+    "GLOGLOW", "GOLDENTEC", "KUU", "HEEPDD", "Adamantiun", "Naroote", "Jectse", 
+    "Heayzoki", "Galaxy", "Motorola", "Xiaomi", "Nokia", "Poco", "realme", "Infinix", 
+    "Blu", "Gshield", "Geonav", "Redmi", "Gorila Shield", "intelbras", "TCL", "Tecno", 
+    "Vbestlife", "MaiJin", "SZAMBIT", "Otterbox", "Sony", "xixaomiro", "Webookers WB", 
+    "PRITOM", "Redmi", "PHILCO", "TCL", "AGM", "Logitech", "DOOGEE", "Blackview", 
+    "HS ACESSÓRIOS", "Coby", "ALLDOCUBE", "Fusion5", "N-one", "bosnyyds", "contixo", 
+    "Headwolf", "C idea", "Ulefone", "Kross Elegance", "Veidoo", "ZONKO", "Hisense", 
+    "Aiwa", "TOSHIBA", "HQ", "ZAPOOS", "TRONOS", "Roku", "Amazon", "NANU SHOP", "ELG", 
+    "Yecna", "Semp", "Zyyini", "Dpofirs", "Supersonic", "Frame My TV", "Tomate", "fosa", 
+    "Coibeu", "Tbest", "BASON LIGHTING"
 ]
