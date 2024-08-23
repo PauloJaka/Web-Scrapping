dcb2d3bc2a5c9d452ef0b8836eef2f68e256be3d
Paulo Jaka <zpaulogamer@gmail.com>
2024-08-23 18:07:29 -0300

    <Refactor> Casas bahia with threads, perfomance up 98% in time

--- E-commerce/Casas_Bahia_web_scrappy.py
+++ E-commerce/Casas_Bahia_web_scrappy.py
@@ -8,6 +8,7 @@ from utils import known_brands
 import os
 import random
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def setup_selenium():
     options = Options()
@@ -16,11 +17,12 @@ def setup_selenium():
     driver = webdriver.Firefox(service=service, options=options)
     return driver
 
-def collect_data_from_casas_bahia(driver, url, current_product, known_brands):
-    driver.get(url)
-    time.sleep(random.uniform(3, 6))  
-
+def collect_data_from_casas_bahia(url, current_product, known_brands):
+    driver = setup_selenium()
+    
     try:
+        driver.get(url)
+        time.sleep(random.uniform(3, 6))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         products = []
         product_elements = soup.select(".dsvia-product-card")
@@ -63,36 +65,36 @@ def collect_data_from_casas_bahia(driver, url, current_product, known_brands):
             except Exception as e:
                 print(f"Error processing item: {e}")
                 continue
+        
+        driver.delete_all_cookies()
     except Exception as e:
         print(f"Error loading page {url}: {e}")
     
+    driver.quit()
     return products
 
-def scrape_casas_bahia(driver, base_url, current_product, num_pages=1):
+def scrape_casas_bahia(base_url, current_product, num_pages=1):
     all_products = []
     
-    for page in range(1, num_pages + 1):
-        url = f"{base_url}?&page={page}"
-        print(f"Scraping page {page}: {url}")
-        products = collect_data_from_casas_bahia(driver, url, current_product, known_brands)
-        all_products.extend(products)
+    urls = [f"{base_url}?&page={page}" for page in range(1, num_pages + 1)]
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(collect_data_from_casas_bahia, url, current_product, known_brands) for url in urls]
         
-        driver.delete_all_cookies()
+        for future in as_completed(futures):
+            all_products.extend(future.result())
     
     return pd.DataFrame(all_products)
 
 def main(products_dict):
     all_data = pd.DataFrame()
-    num_pages = 2
-    driver = setup_selenium()
-    time.sleep(5)
+    num_pages = 10
     
-    try: 
-        for product, base_url in products_dict.items():
-            df = scrape_casas_bahia(driver, base_url, product, num_pages)
-            all_data = pd.concat([all_data, df], ignore_index=True)
-    finally:
-        driver.quit()
+    with ThreadPoolExecutor(max_workers=len(products_dict)) as executor:
+        futures = [executor.submit(scrape_casas_bahia, base_url, product, num_pages) for product, base_url in products_dict.items()]
+        
+        for future in as_completed(futures):
+            all_data = pd.concat([all_data, future.result()], ignore_index=True)
 
     if all_data.empty:
         print("Empty data")
@@ -102,8 +104,8 @@ def main(products_dict):
 if __name__ == "__main__":
     products_dict = {
         "Notebook": "https://www.casasbahia.com.br/Notebook/b?origem=topterms",
-        #"Smartphones": "https://www.casasbahia.com.br/Smartphones/b?origem=topterms",
-        #"TV": "https://www.casasbahia.com.br/TV/b",
-        #"Tablet": "https://www.casasbahia.com.br/tablet/b"
+        "Smartphones": "https://www.casasbahia.com.br/Smartphones/b?origem=topterms",
+        "TV": "https://www.casasbahia.com.br/TV/b",
+        "Tablet": "https://www.casasbahia.com.br/tablet/b"
     }
     main(products_dict)
