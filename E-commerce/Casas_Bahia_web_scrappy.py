f932f9635e1a854eb18299b66569376c290922a1
Paulo Jaka <zpaulogamer@gmail.com>
2024-09-07 21:45:50 -0300

    <Refactor> casas bahia refactor, css selector on selenium

--- E-commerce/Casas_Bahia_web_scrappy.py
+++ E-commerce/Casas_Bahia_web_scrappy.py
@@ -1,14 +1,14 @@
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime
-from utils import known_brands
+from datetime import datetime, timedelta
+from utils.utils import known_brands
 import os
 import random
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def setup_selenium():
     options = Options()
@@ -17,9 +17,7 @@ def setup_selenium():
     driver = webdriver.Firefox(service=service, options=options)
     return driver
 
-def collect_data_from_casas_bahia(url, current_product, known_brands):
-    driver = setup_selenium()
-    
+def collect_data_from_casas_bahia(driver, url, current_product, known_brands):
     try:
         driver.get(url)
         time.sleep(random.uniform(3, 6))
@@ -34,8 +32,8 @@ def collect_data_from_casas_bahia(url, current_product, known_brands):
         for item in product_elements:
             try:
                 title_element = item.select_one(".product-card__title a")
-                original_price_element = item.select_one(".product-card__price-value")
-                discount_price_element = item.select_one(".product-card__discount-price")
+                original_price_element = item.select_one('b[aria-hidden="true"]')
+                discount_price_element = item.select_one(".product-card__highlight-price")
                 link_element = item.select_one(".product-card__title a")['href']
             
                 title = title_element.text.strip() if title_element else ""
@@ -48,49 +46,61 @@ def collect_data_from_casas_bahia(url, current_product, known_brands):
                     if known_brand.lower() in title.lower():
                         brand = known_brand
                         break
+            
+                discount_price = discount_price.replace('.', '').replace(',', '.') if discount_price else None
+                original_price = original_price.replace('.', '').replace(',', '.') if original_price else None
 
                 products.append({
                     'title': title,
-                    'price_original': original_price,
-                    'price_discount': discount_price,
+                    'original_price': original_price,
+                    'discount_price': discount_price,
                     'brand': brand,
                     'link': link,
-                    'rating': round(random.uniform(3.5, 5.0), 1),
+                    'rating': round(random.uniform(4.0, 5.0), 1),
                     'free_freight': False,
-                    'product': current_product,
-                    'CreatedAt': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    'UpdatedAt': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    'category': current_product,
+                    'created_at': (datetime.now() - timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S'),
+                    'updated_at': (datetime.now() - timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S'),
                     'website': 'Casas Bahia'
                 })
             except Exception as e:
                 print(f"Error processing item: {e}")
                 continue
-        
-        driver.delete_all_cookies()
     except Exception as e:
-        print(f"Error loading page {url}: {e}")
+        print(f"Error loading page {url} : {e}")
     
-    driver.quit()
     return products
 
 def scrape_casas_bahia(base_url, current_product, num_pages=1):
+    driver = setup_selenium()
     all_products = []
-    
-    urls = [f"{base_url}?&page={page}" for page in range(1, num_pages + 1)]
 
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(collect_data_from_casas_bahia, url, current_product, known_brands) for url in urls]
-        
-        for future in as_completed(futures):
-            all_products.extend(future.result())
+    try:
+        urls = [f"{base_url}?&page={page}" for page in range(1, num_pages + 1)]
+
+        for url in urls:
+            products = collect_data_from_casas_bahia(driver, url, current_product, known_brands)
+            all_products.extend(products)
+            driver.delete_all_cookies() 
     
+    finally:
+        driver.quit()
+
     return pd.DataFrame(all_products)
 
-def main(products_dict):
+def Casas_Bahia_Scrappy_Products():
     all_data = pd.DataFrame()
     num_pages = 10
     
-    with ThreadPoolExecutor(max_workers=len(products_dict)) as executor:
+    products_dict = {
+        "Notebook": "https://www.casasbahia.com.br/Notebook/b?origem=topterms",
+        "Smartphones": "https://www.casasbahia.com.br/Smartphones/b?origem=topterms",
+        "TV": "https://www.casasbahia.com.br/TV/b",
+        "Tablet": "https://www.casasbahia.com.br/tablet/b",
+        "Smartwatch": "https://www.casasbahia.com.br/Smartwatch/b"
+    }
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
         futures = [executor.submit(scrape_casas_bahia, base_url, product, num_pages) for product, base_url in products_dict.items()]
         
         for future in as_completed(futures):
@@ -100,12 +110,7 @@ def main(products_dict):
         print("Empty data")
     else:
         print(all_data.to_string(index=False))
+        return all_data
 
 if __name__ == "__main__":
-    products_dict = {
-        "Notebook": "https://www.casasbahia.com.br/Notebook/b?origem=topterms",
-        "Smartphones": "https://www.casasbahia.com.br/Smartphones/b?origem=topterms",
-        "TV": "https://www.casasbahia.com.br/TV/b",
-        "Tablet": "https://www.casasbahia.com.br/tablet/b"
-    }
-    main(products_dict)
+    Casas_Bahia_Scrappy_Products()
